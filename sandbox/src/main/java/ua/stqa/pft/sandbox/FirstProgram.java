2ee904287341bf9a4b948277f4ce24e510f26455
orchid17 <orchid_17@mail.ru>
2016-12-19 21:11:32 +0000

    Добавлена программа расчета расстояния между точками версия1

--- sandbox/src/main/java/ua/stqa/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/FirstProgram.java
@@ -4,8 +4,14 @@ public class FirstProgram {
 
 
    public static void main(String[] args){
-       hello("world");
-       hello("user");
+
+       Point p1 = new Point (1,2);
+
+       Point p2 = new Point (1,3);
+
+       System.out.println("Координаты точки p1: " + p1.x + " и "+ p1.y);
+       System.out.println("Координаты точки p2: " + p2.x + " и "+ p2.y);
+       System.out.println("Расстояние между точками:  "+ Distance(p1, p2));
 
        double l = 5;
        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
@@ -16,10 +22,11 @@ public class FirstProgram {
 
    }
 
-    public static void hello (String somebody){
-        System.out.println("Hello, " + somebody);
-
+    public static double Distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x),2)+Math.pow((p2.y - p1.y),2));
     }
+
+
 
     public  static double area (double len){
         return len * len;
---
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ua.stqa.pft.sandbox;
+
+/**
+ * Created by Оля on 19.12.2016.
+ */
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
